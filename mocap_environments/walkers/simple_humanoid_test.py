9624efc9b111286f823d47118beeb16b3006116c
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2023-06-15 13:47:44 +0100

    Replace CMU -> simple
    ```sh
    our_python_root="./mocap_environments"
    
    find "$our_python_root/walkers" \
        -type f \
        -name "*.py" \
        -exec sed -i "" -e 's#CMU humanoid#simple humanoid#g' {} \;
    
    find "$our_python_root/walkers" \
        -type f \
        -name "*.py" \
        -exec sed -i "" -e 's#cmu_#simple_#g' {} \;
    
    find "$our_python_root/walkers" \
        -type f \
        -name "*.py" \
        -exec sed -i "" -e 's#cmu_humanoid#simple_humanoid#g' {} \;
    
    find "$our_python_root/walkers" \
        -type f \
        -name "*.py" \
        -exec sed -i "" -e 's#CMUHumanoid#SimpleHumanoid#g' {} \;
    
    find "$our_python_root/walkers" \
        -type f \
        -name "*.py" \
        -exec sed -i "" -e 's#_CMU_MOCAP_JOINTS#_MOCAP_JOINTS#g' {} \;
    
    find "$our_python_root/walkers" \
        -type f \
        -name "*.py" \
        -exec sed -i "" -e 's#cmu_pose_to_actuation#pose_to_actuation#g' {} \;
    
    sed \
        -i "" \
        -e 's#from dm_control.locomotion.walkers import cmu_humanoid#from . import simple_humanoid#g' \
        "$our_python_root/walkers/simple_humanoid_test.py"
    
    sed \
        -i "" \
        -e 's#"assets/humanoid_CMU_V{model_version}.xml"#"simple_humanoid.xml"#g' \
        "$our_python_root/walkers/simple_humanoid.py"
    
    cat << EOF > "$our_python_root/walkers/__init__.py"
    """Walkers for motion tracking."""
    
    from . import simple_humanoid
    
    SimpleHumanoid = simple_humanoid.SimpleHumanoid
    SimpleHumanoidPositionControlled = simple_humanoid.SimpleHumanoidPositionControlled
    EOF
    
    isort . && black .
    ```

--- mocap_environments/walkers/simple_humanoid_test.py
+++ mocap_environments/walkers/simple_humanoid_test.py
@@ -1,19 +1,19 @@
-"""Tests for the CMU humanoid."""
+"""Tests for the simple humanoid."""
 
 
 from absl.testing import absltest
 from absl.testing import parameterized
 from dm_control import mjcf
 from dm_control.composer.observation.observable import base as observable_base
-from dm_control.locomotion.walkers import cmu_humanoid
+from dm_control.locomotion.walkers import simple_humanoid
 import numpy as np
 
 
-class CMUHumanoidTest(parameterized.TestCase):
+class SimpleHumanoidTest(parameterized.TestCase):
     @parameterized.parameters(
         [
-            cmu_humanoid.CMUHumanoid,
-            cmu_humanoid.CMUHumanoidPositionControlled,
+            simple_humanoid.SimpleHumanoid,
+            simple_humanoid.SimpleHumanoidPositionControlled,
         ]
     )
     def test_can_compile_and_step_simulation(self, walker_type):
@@ -24,8 +24,8 @@ class CMUHumanoidTest(parameterized.TestCase):
 
     @parameterized.parameters(
         [
-            cmu_humanoid.CMUHumanoid,
-            cmu_humanoid.CMUHumanoidPositionControlled,
+            simple_humanoid.SimpleHumanoid,
+            simple_humanoid.SimpleHumanoidPositionControlled,
         ]
     )
     def test_actuators_sorted_alphabetically(self, walker_type):
@@ -36,31 +36,33 @@ class CMUHumanoidTest(parameterized.TestCase):
         np.testing.assert_array_equal(actuator_names, sorted(actuator_names))
 
     def test_actuator_to_mocap_joint_mapping(self):
-        walker = cmu_humanoid.CMUHumanoid()
+        walker = simple_humanoid.SimpleHumanoid()
 
         with self.subTest("Forward mapping"):
-            for actuator_num, cmu_mocap_joint_num in enumerate(walker.actuator_order):
+            for actuator_num, simple_mocap_joint_num in enumerate(
+                walker.actuator_order
+            ):
                 self.assertEqual(
-                    walker.actuator_to_joint_order[cmu_mocap_joint_num], actuator_num
+                    walker.actuator_to_joint_order[simple_mocap_joint_num], actuator_num
                 )
 
         with self.subTest("Inverse mapping"):
-            for cmu_mocap_joint_num, actuator_num in enumerate(
+            for simple_mocap_joint_num, actuator_num in enumerate(
                 walker.actuator_to_joint_order
             ):
                 self.assertEqual(
-                    walker.actuator_order[actuator_num], cmu_mocap_joint_num
+                    walker.actuator_order[actuator_num], simple_mocap_joint_num
                 )
 
-    def test_cmu_humanoid_position_controlled_has_correct_actuators(self):
-        walker_torque = cmu_humanoid.CMUHumanoid()
-        walker_pos = cmu_humanoid.CMUHumanoidPositionControlled()
+    def test_simple_humanoid_position_controlled_has_correct_actuators(self):
+        walker_torque = simple_humanoid.SimpleHumanoid()
+        walker_pos = simple_humanoid.SimpleHumanoidPositionControlled()
 
         actuators_torque = walker_torque.mjcf_model.find_all("actuator")
         actuators_pos = walker_pos.mjcf_model.find_all("actuator")
 
         actuator_pos_params = {
-            params.name: params for params in cmu_humanoid._POSITION_ACTUATORS
+            params.name: params for params in simple_humanoid._POSITION_ACTUATORS
         }
 
         self.assertEqual(len(actuators_torque), len(actuators_pos))
@@ -92,7 +94,7 @@ class CMUHumanoidTest(parameterized.TestCase):
         ]
     )
     def test_get_element_property(self, name):
-        attribute_value = getattr(cmu_humanoid.CMUHumanoid(), name)
+        attribute_value = getattr(simple_humanoid.SimpleHumanoid(), name)
         self.assertIsInstance(attribute_value, mjcf.Element)
 
     @parameterized.parameters(
@@ -106,19 +108,19 @@ class CMUHumanoidTest(parameterized.TestCase):
         ]
     )
     def test_get_element_tuple_property(self, name):
-        attribute_value = getattr(cmu_humanoid.CMUHumanoid(), name)
+        attribute_value = getattr(simple_humanoid.SimpleHumanoid(), name)
         self.assertNotEmpty(attribute_value)
         for item in attribute_value:
             self.assertIsInstance(item, mjcf.Element)
 
     def test_set_name(self):
         name = "fred"
-        walker = cmu_humanoid.CMUHumanoid(name=name)
+        walker = simple_humanoid.SimpleHumanoid(name=name)
         self.assertEqual(walker.mjcf_model.model, name)
 
     def test_set_marker_rgba(self):
         marker_rgba = (1.0, 0.0, 1.0, 0.5)
-        walker = cmu_humanoid.CMUHumanoid(marker_rgba=marker_rgba)
+        walker = simple_humanoid.SimpleHumanoid(marker_rgba=marker_rgba)
         for marker_geom in walker.marker_geoms:
             np.testing.assert_array_equal(marker_geom.rgba, marker_rgba)
 
@@ -130,33 +132,33 @@ class CMUHumanoidTest(parameterized.TestCase):
         "sensors_torque",
     )
     def test_evaluate_observable(self, name):
-        walker = cmu_humanoid.CMUHumanoid()
+        walker = simple_humanoid.SimpleHumanoid()
         observable = getattr(walker.observables, name)
         physics = mjcf.Physics.from_mjcf_model(walker.mjcf_model)
         observation = observable(physics)
         self.assertIsInstance(observation, (float, np.ndarray))
 
     def test_proprioception(self):
-        walker = cmu_humanoid.CMUHumanoid()
+        walker = simple_humanoid.SimpleHumanoid()
         for item in walker.observables.proprioception:
             self.assertIsInstance(item, observable_base.Observable)
 
-    def test_cmu_pose_to_actuation(self):
-        walker = cmu_humanoid.CMUHumanoidPositionControlled()
+    def test_simple_pose_to_actuation(self):
+        walker = simple_humanoid.SimpleHumanoidPositionControlled()
         random_state = np.random.RandomState(123)
 
         expected_actuation = random_state.uniform(-1, 1, len(walker.actuator_order))
 
-        cmu_limits = zip(*(joint.range for joint in walker.mocap_joints))
-        cmu_lower, cmu_upper = (np.array(limit) for limit in cmu_limits)
-        cmu_pose = (
-            cmu_lower
-            + (cmu_upper - cmu_lower)
+        simple_limits = zip(*(joint.range for joint in walker.mocap_joints))
+        simple_lower, simple_upper = (np.array(limit) for limit in simple_limits)
+        simple_pose = (
+            simple_lower
+            + (simple_upper - simple_lower)
             * (1 + expected_actuation[walker.actuator_to_joint_order])
             / 2
         )
 
-        actual_actuation = walker.cmu_pose_to_actuation(cmu_pose)
+        actual_actuation = walker.simple_pose_to_actuation(simple_pose)
 
         np.testing.assert_allclose(actual_actuation, expected_actuation)
 
