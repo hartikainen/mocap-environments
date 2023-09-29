d0b0757f417e8276089e385fd4c3f7e1b8dc4066
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2023-09-29 16:00:29 +0000

    Fix simple humanoid amass data loader

--- mocap_environments/data/simple_humanoid_amass.py
+++ mocap_environments/data/simple_humanoid_amass.py
@@ -2,40 +2,29 @@
 
 import pathlib
 import re
+from typing import Optional
 
 import numpy as np
+import numpy.typing as npt
 import tensorflow as tf
 
 Path = pathlib.Path
 
 
-def reset_xy_position(keyframes):
-    # Set the keyframes to begin from origin.
-    initial_root_xyz = tf.gather_nd(
-        keyframes, tf.zeros(tf.rank(keyframes) - 1, dtype=tf.int32)
-    )
-    keyframes = tf.concat(  # pylint: disable=E1123,E1120
-        [keyframes[..., :2] - initial_root_xyz[..., :2], keyframes[..., 2:]],
-        axis=-1,
-    )
-
-    return keyframes
-
-
 def preprocess_motion(x):
-    x["keyframes"] = reset_xy_position(x["keyframes"])
-    # TODO(hartikainen): Make this configurable and more transparent. These
-    # values depend on the joint ordering in the the AMASS dataset.
-    # fmt: off
-    smpl_to_simple_humanoid_indices = [
-        15, 0, 2, 5, 8, 11, 1, 4, 7, 10, 17, 19, 21, 16, 18, 20,
-    ]
-    # fmt: on
-    num_joints = tf.shape(x["keyframes"])[-2]
-    if tf.equal(num_joints, 24):
-        x["keyframes"] = tf.gather(
-            x["keyframes"], smpl_to_simple_humanoid_indices, axis=-2
-        )
+    # # TODO(hartikainen): Make this configurable and more transparent. These
+    # # values depend on the joint ordering in the the AMASS dataset.
+    # # fmt: off
+    # smpl_to_simple_humanoid_indices = [
+    #     15, 0, 2, 5, 8, 11, 1, 4, 7, 10, 17, 19, 21, 16, 18, 20,
+    # ]
+
+    # # fmt: on
+    # num_joints = tf.shape(x["keyframes"])[-2]
+    # if tf.equal(num_joints, 24):
+    #     x["keyframes"] = tf.gather(
+    #         x["keyframes"], smpl_to_simple_humanoid_indices, axis=-2
+    #     )
 
     num_joints = tf.shape(x["keyframes"])[-2]
     tf.debugging.assert_equal(num_joints, 16)
@@ -45,20 +34,29 @@ def preprocess_motion(x):
 
 def read_motion_file(
     motion_file: tf.Tensor,
-) -> tuple[np.array, np.array, np.array, np.array]:
+) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     """Load the motion file from the `npy`-serialized `motion_file`."""
-    motion_file = Path(motion_file.numpy().decode())
-    with motion_file.open("rb") as f:
+    motion_file_path = Path(motion_file.numpy().decode())
+    with motion_file_path.open("rb") as f:
         motion = np.load(f)
-        assert {*motion.files} == {
-            "keyframes",
-            "qpos",
-            "qvel",
-            "mocap_id",
-        }, motion.files
         motion = dict(motion)
+        # expected_keys = {
+        #     "qpos",
+        #     "qvel",
+        #     "act",
+        #     "act_dot",
+        #     "ctrl",
+        #     "keyframes",
+        #     "mocap_id",
+        # }
+        # if set(motion.keys()) != expected_keys:
+        #     raise ValueError(
+        #         f"Something wrong with the motion '{str(motion_file_path)}'. Expected motion to "
+        #         f"contain keys '{expected_keys}' but got '{set(motion.keys())}'."
+        #     )
+        motion = {key: motion[key] for key in {"keyframes", "qpos", "qvel", "mocap_id"}}
 
-    assert motion["keyframes"].ndim == 3, motion["keyframes"].ndim
+    np.testing.assert_equal(motion["keyframes"].ndim, 3)
     num_timesteps, num_joints = motion["keyframes"].shape[:2]
     np.testing.assert_equal(motion["mocap_id"].shape, ())
     np.testing.assert_equal(motion["keyframes"].shape, (num_timesteps, num_joints, 3))
@@ -80,7 +78,8 @@ def read_motion_file(
 
 def load_dataset(
     data_path: Path,
-    mocap_id_filter_regex=None,
+    mocap_id_filter_regex: Optional[str] = None,
+    shuffle_files: bool = False,
 ) -> tf.data.Dataset:
     """Load a motion dataset from `data_path` in `tf.data.Dataset` form."""
     # TODO(mjpc-dagger): Might have to change this `glob` based on the file library.
@@ -88,13 +87,15 @@ def load_dataset(
 
     if mocap_id_filter_regex is not None:
         mocap_id_pattern = re.compile(mocap_id_filter_regex)
-        motion_file_paths = tuple(
+        motion_file_paths = [
             x
             for x in motion_file_paths
             if re.match(mocap_id_pattern, str(x.relative_to(data_path)))
-        )
+        ]
 
     dataset = tf.data.Dataset.from_tensor_slices(list(map(str, motion_file_paths)))
+    if shuffle_files:
+        dataset = dataset.shuffle(dataset.cardinality())
     # NOTE(hartikainen): `tf.py_function` doesn't allow `dict` outputs so we map
     # to tuples and unpack them in the subsequent `map`.
     dataset = dataset.map(
