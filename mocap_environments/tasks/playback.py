e1f080570ae5f1983f71cd6edd5a20cd58b89895
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2023-10-01 12:40:20 +0000

    Add motion playback task and environment

---
+++ mocap_environments/tasks/playback.py
@@ -0,0 +1,23 @@
+"""Motion playback `dm_control.composer.Task`."""
+
+from dm_control import mjcf
+import numpy as np
+
+from mocap_environments.tasks import tracking as tracking_task
+
+
+class PlaybackTask(tracking_task.TrackingTask):
+    """Task that simply replays the motion sequences."""
+
+    def before_step(
+        self,
+        physics: mjcf.Physics,
+        action: np.ndarray,
+        random_state: np.random.RandomState,
+    ):
+        del physics, action, random_state
+
+    def after_step(self, physics: mjcf.Physics, random_state: np.random.RandomState):
+        result = super().after_step(physics, random_state)
+        self._set_walker(physics)
+        return result
