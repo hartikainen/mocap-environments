397fe3fef53d1e56660c6978e77b15e454f36113
Kristian Hartikainen <kristian.hartikainen@gmail.com>
2024-02-02 16:47:18 +0000

    Move motion tracking task xml

--- mocap_environments/environments/humanoid_motion_playback.py
+++ mocap_environments/environments/humanoid_motion_playback.py
@@ -2,6 +2,7 @@
 
 # pylint: disable=duplicate-code
 
+import pathlib
 from typing import Literal, Optional
 
 from dm_control import composer
@@ -10,6 +11,9 @@ import numpy as np
 
 from mocap_environments.tasks import playback as playback_task
 from mocap_environments.walkers import simple_humanoid as simple_humanoid_walker
+
+
+Path = pathlib.Path
 
 
 def load(
@@ -21,12 +25,24 @@ def load(
     random_state: Optional[np.random.RandomState] = None,
     task_kwargs,
 ) -> composer.Environment:
-    """Helper constructor for a mocap tracking environment."""
+    """Helper constructor for a mocap playback environment."""
 
     if walker_type == "SimpleHumanoidPositionControlled":
         walker_class = simple_humanoid_walker.SimpleHumanoidPositionControlled
+        mjpc_task_xml_file_path = (
+            Path(__file__).parent.parent
+            / "tasks"
+            / "mjpc_tasks"
+            / "simple_humanoid_motion_tracking.xml"
+        )
     elif walker_type == "SimpleHumanoid":
         walker_class = simple_humanoid_walker.SimpleHumanoid
+        mjpc_task_xml_file_path = (
+            Path(__file__).parent.parent
+            / "tasks"
+            / "mjpc_tasks"
+            / "simple_humanoid_motion_tracking.xml"
+        )
     else:
         raise ValueError(f"{walker_type=}")
 
@@ -35,6 +51,7 @@ def load(
     task = playback_task.PlaybackTask(
         walker=walker_class,
         arena=arena,
+        mjpc_task_xml_file_path=mjpc_task_xml_file_path,
         **task_kwargs,
     )
 
--- mocap_environments/environments/humanoid_motion_tracking.py
+++ mocap_environments/environments/humanoid_motion_tracking.py
@@ -2,6 +2,7 @@
 
 # pylint: disable=duplicate-code
 
+import pathlib
 from typing import Literal, Optional
 
 from dm_control import composer
@@ -10,6 +11,9 @@ import numpy as np
 
 from mocap_environments.tasks import tracking as tracking_task
 from mocap_environments.walkers import simple_humanoid as simple_humanoid_walker
+
+
+Path = pathlib.Path
 
 
 def load(
@@ -25,8 +29,20 @@ def load(
 
     if walker_type == "SimpleHumanoidPositionControlled":
         walker_class = simple_humanoid_walker.SimpleHumanoidPositionControlled
+        mjpc_task_xml_file_path = (
+            Path(__file__).parent.parent
+            / "tasks"
+            / "mjpc_tasks"
+            / "simple_humanoid_motion_tracking.xml"
+        )
     elif walker_type == "SimpleHumanoid":
         walker_class = simple_humanoid_walker.SimpleHumanoid
+        mjpc_task_xml_file_path = (
+            Path(__file__).parent.parent
+            / "tasks"
+            / "mjpc_tasks"
+            / "simple_humanoid_motion_tracking.xml"
+        )
     else:
         raise ValueError(f"{walker_type=}")
 
@@ -35,6 +51,7 @@ def load(
     task = tracking_task.TrackingTask(
         walker=walker_class,
         arena=arena,
+        mjpc_task_xml_file_path=mjpc_task_xml_file_path,
         **task_kwargs,
     )
 
